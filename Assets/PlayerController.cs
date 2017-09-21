4938a147893b2163816f2ac2d563851880f3f8b2
Gengee <Jtapia23@mail.pima.edu>
2017-09-21 15:44:25 -0700

    Character Controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float speed = 7;
+    private Vector3 movement;
+    public Rigidbody player;
+
+    void Start() {
+    }
+
+    void Update() {
+        movement.z = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        player.velocity = movement * speed;
+    }
 }
