86971cd1239ee9c3168033862f86315d6d0d7125
Gengee <Jtapia23@mail.pima.edu>
2017-09-22 11:12:07 -0700

    Anime Player, Wall, and KeyDoor

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,14 +6,27 @@ public class PlayerController : MonoBehaviour {
 
     private float speed = 7;
     private Vector3 movement;
+    private Vector3 lastPos;
+    private bool burning = false;
     public Rigidbody player;
 
     void Start() {
+        movement.z = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        lastPos = movement;
     }
 
     void Update() {
         movement.z = Input.GetAxis("Vertical");
         movement.x = Input.GetAxis("Horizontal");
         player.velocity = movement * speed;
+        if (movement != lastPos) {
+            player.transform.rotation = Quaternion.LookRotation(movement - lastPos);
+            GetComponent<Animator>().SetBool("Walking", true);
+        }
+        else {
+            lastPos = movement;
+            GetComponent<Animator>().SetBool("Walking", false);
+        }
     }
 }
