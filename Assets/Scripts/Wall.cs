7c31a2c5d64c7908b66362da4ef88e7a40e47af7
DESKTOP-1SV3R20\jenma <flirn2@gmail.com>
2017-10-09 16:26:01 -0700

    added the Wall scriptable object

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Wall", menuName = "Wall")]
+public class Wall : ScriptableObject
+{
+    public List<Vector3> wallPos = new List<Vector3> { new Vector3(18, 0, 5), new Vector3(18, 0, 8), new Vector3(18, 0, 11) };
+    public List<Quaternion> wallRot = new List<Quaternion> { new Quaternion(0, 0, 0, 0), new Quaternion(0, 0, 0, 0), new Quaternion(0, 0, 0, 0) };
+}
